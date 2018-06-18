9f040f93fcc89f1e32d3d27a97d793e17c5b731d
nan <liu.junnan@outlook.com>
2018-06-18 21:10:06 +0800

    PracticalLisp

--- books/lisp/PracticalLisp/packages/macro-utils/macro-utils.lisp
+++ books/lisp/PracticalLisp/packages/macro-utils/macro-utils.lisp
@@ -4,3 +4,10 @@
   `(let ,(loop for n in names collect `(,n (make-symbol ,(string n))))
      ,@body))
 
+
+(defmacro once-only ((&rest names) &body body)
+  (let ((gensyms (loop for n in names collect (gensym (string n)))))
+    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
+      `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
+        ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
+           ,@body)))))
